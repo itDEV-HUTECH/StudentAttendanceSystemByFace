73af20b15429f508eb956a34583d7ca08382a0a4
Hieu Pham <minhhieupham2002@gmail.com>
2023-10-04 18:43:14 +0700

    refactor code

--- main/view/lecturer_views.py
+++ main/view/lecturer_views.py
@@ -17,138 +17,118 @@ def lecturer_dashboard_view(request):
 
 @lecturer_required
 def lecturer_schedule_view(request):
-    id_staff = request.session.get('id_staff')
+    id_lecturer = request.session.get('id_staff')
     week_start_param = request.GET.get('week_start')
 
-    if id_staff is not None:
-        if week_start_param:
-            try:
-                week_start = date.fromisoformat(week_start_param)
-            except ValueError:
-                raise Http404("Invalid date format for week_start parameter")
-        else:
-            today = date.today()
-            week_start = today - timedelta(days=today.weekday())
-
-        end_of_week = week_start + timedelta(days=6)
-
-        lecturer_classes = Classroom.objects.filter(
-            id_lecturer__id_staff=id_staff,
-            begin_date__lte=end_of_week,
-            end_date__gte=week_start
-        ).order_by('day_of_week_begin', 'begin_time')
-
-        previous_week_start = week_start - timedelta(days=7)
-        next_week_start = week_start + timedelta(days=7)
-
-        previous_week_start = previous_week_start.strftime("%Y-%m-%d")
-        next_week_start = next_week_start.strftime("%Y-%m-%d")
-
-        context = {
-            'lecturer_classes': lecturer_classes,
-            'start_of_week': week_start,
-            'end_of_week': end_of_week,
-            'previous_week_start': previous_week_start,
-            'next_week_start': next_week_start,
-        }
-        return render(request, 'lecturer/lecturer_schedule.html', context)
+    if week_start_param:
+        try:
+            week_start = date.fromisoformat(week_start_param)
+        except ValueError:
+            raise Http404("Invalid date format for week_start parameter")
     else:
-        request.session['next_url'] = request.path
-        return redirect('login')
+        today = date.today()
+        week_start = today - timedelta(days=today.weekday())
+
+    end_of_week = week_start + timedelta(days=6)
+
+    lecturer_classes = Classroom.objects.filter(
+        id_lecturer__id_staff=id_lecturer,
+        begin_date__lte=end_of_week,
+        end_date__gte=week_start
+    ).order_by('day_of_week_begin', 'begin_time')
+
+    previous_week_start = week_start - timedelta(days=7)
+    next_week_start = week_start + timedelta(days=7)
+
+    previous_week_start = previous_week_start.strftime("%Y-%m-%d")
+    next_week_start = next_week_start.strftime("%Y-%m-%d")
+
+    context = {
+        'lecturer_classes': lecturer_classes,
+        'start_of_week': week_start,
+        'end_of_week': end_of_week,
+        'previous_week_start': previous_week_start,
+        'next_week_start': next_week_start,
+    }
+    return render(request, 'lecturer/lecturer_schedule.html', context)
 
 
 @lecturer_required
 def lecturer_profile_view(request):
-    if 'id_staff' in request.session:
-        id_staff = request.session['id_staff']
-        try:
-            lecturer = StaffInfo.objects.get(id_staff=id_staff)
+    id_lecturer = request.session['id_staff']
+    lecturer = StaffInfo.objects.get(id_staff=id_lecturer)
 
-            if request.method == 'POST':
-                lecturer.staff_name = request.POST['lecturer_name']
-                lecturer.email = request.POST['email']
-                lecturer.phone = request.POST['phone']
-                lecturer.address = request.POST['address']
-                lecturer.birthday = datetime.strptime(request.POST['birthday'], '%d/%m/%Y').date()
-                lecturer.save()
-                messages.success(request, 'Thay đổi thông tin thành công.')
-            context = {'lecturer': lecturer}
-            return render(request, 'lecturer/lecturer_profile.html', context)
-        except StaffInfo.DoesNotExist:
-            return redirect('login')
-    else:
-        request.session['next_url'] = request.path
-        return redirect('login')
+    if request.method == 'POST':
+        lecturer.staff_name = request.POST['lecturer_name']
+        lecturer.email = request.POST['email']
+        lecturer.phone = request.POST['phone']
+        lecturer.address = request.POST['address']
+        lecturer.birthday = datetime.strptime(request.POST['birthday'], '%d/%m/%Y').date()
+        lecturer.save()
+        messages.success(request, 'Thay đổi thông tin thành công.')
+
+    context = {'lecturer': lecturer}
+
+    return render(request, 'lecturer/lecturer_profile.html', context)
 
 
 @lecturer_required
 def lecturer_change_password_view(request):
-    if 'id_staff' in request.session:
-        id_staff = request.session['id_staff']
+    id_lecturer = request.session['id_staff']
+    lecturer = StaffInfo.objects.get(id_staff=id_lecturer)
 
-        try:
-            lecturer = StaffInfo.objects.get(id_staff=id_staff)
+    if request.method == 'POST':
+        old_password = request.POST['old_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
 
-            if request.method == 'POST':
-                old_password = request.POST['old_password']
-                new_password = request.POST['new_password']
-                confirm_password = request.POST['confirm_password']
+        if check_password(old_password, lecturer.password):
+            if new_password == confirm_password:
+                lecturer.password = make_password(new_password)
+                lecturer.save()
+                update_session_auth_hash(request, lecturer)
+                messages.success(request, 'Đổi mật khẩu thành công.')
+            else:
+                messages.error(request, 'Mật khẩu mới không khớp.')
+        else:
+            messages.error(request, 'Mật khẩu cũ không đúng.')
 
-                if check_password(old_password, lecturer.password):
-                    if new_password == confirm_password:
-                        lecturer.password = make_password(new_password)
-                        lecturer.save()
-                        update_session_auth_hash(request, lecturer)
-                        messages.success(request, 'Đổi mật khẩu thành công.')
-                    else:
-                        messages.error(request, 'Mật khẩu mới không khớp.')
-                else:
-                    messages.error(request, 'Mật khẩu cũ không đúng.')
-
-            return render(request, 'lecturer/lecturer_change_password.html')
-
-        except StaffInfo.DoesNotExist:
-            return redirect('login')
-    else:
-        request.session['next_url'] = request.path
-        return redirect('login')
+    return render(request, 'lecturer/lecturer_change_password.html')
 
 
 @lecturer_required
 def lecturer_attendance_class_view(request):
     id_staff = request.session.get('id_staff')
 
-    if id_staff is not None:
-        today = date.today()
-        week_start = today - timedelta(days=today.weekday())
-        end_of_week = week_start + timedelta(days=6)
+    today = date.today()
+    week_start = today - timedelta(days=today.weekday())
+    end_of_week = week_start + timedelta(days=6)
 
-        lecturer_classes = Classroom.objects.filter(
-            id_lecturer__id_staff=id_staff,
-            begin_date__lte=end_of_week,
-            end_date__gte=week_start
-        ).order_by('day_of_week_begin', 'begin_time')
+    lecturer_classes = Classroom.objects.filter(
+        id_lecturer__id_staff=id_staff,
+        begin_date__lte=end_of_week,
+        end_date__gte=week_start
+    ).order_by('day_of_week_begin', 'begin_time')
 
-        day_of_week_today = today.isoweekday()
+    day_of_week_today = today.isoweekday()
 
-        context = {
-            'lecturer_classes': lecturer_classes,
-            'start_of_week': week_start,
-            'end_of_week': end_of_week,
-            'day_of_week_today': day_of_week_today,
-        }
+    context = {
+        'lecturer_classes': lecturer_classes,
+        'start_of_week': week_start,
+        'end_of_week': end_of_week,
+        'day_of_week_today': day_of_week_today,
+    }
 
-        return render(request, 'lecturer/lecturer_attendance_class.html', context)
-    else:
-        request.session['next_url'] = request.path
-        return redirect('login')
+    return render(request, 'lecturer/lecturer_attendance_class.html', context)
 
 
+@lecturer_required
 def lecturer_mark_attendance(request, classroom_id):
     classroom = Classroom.objects.get(pk=classroom_id)
     students_in_class = StudentClassDetails.objects.filter(id_classroom=classroom)
     attendance_list = Attendance.objects.filter(id_classroom=classroom)
     day_of_week_today = date.today().isoweekday()
+
     if day_of_week_today != classroom.day_of_week_begin:
         return redirect('lecturer_attendance')
     elif request.method == 'POST':
--- main/view/student_views.py
+++ main/view/student_views.py
@@ -11,11 +11,7 @@ from main.models import StudentInfo, Classroom
 
 
 def student_login_view(request):
-    if 'id_student' in request.session:
-        return redirect('student_dashboard')
-
     error_message = None
-    next_url = request.session.get('next_url')
     if request.method == 'POST':
         id_student = request.POST.get('id_student')
         password = request.POST.get('password')
@@ -24,10 +20,7 @@ def student_login_view(request):
             student = StudentInfo.objects.get(id_student=id_student)
             if check_password(password, student.password):
                 request.session['id_student'] = student.id_student
-                if next_url:
-                    return redirect(next_url)
-                else:
-                    return redirect('student_dashboard')
+                return redirect('student_dashboard')
             else:
                 error_message = "Tên đăng nhập hoặc mật khẩu không đúng."
         except StudentInfo.DoesNotExist:
@@ -38,10 +31,7 @@ def student_login_view(request):
 
 @student_required
 def student_dashboard_view(request):
-    if 'id_student' in request.session:
-        return render(request, 'student/student_home.html')
-    else:
-        return redirect('student_login')
+    return render(request, 'student/student_home.html')
 
 
 @student_required
@@ -49,95 +39,78 @@ def student_schedule_view(request):
     id_student = request.session.get('id_student')
     week_start_param = request.GET.get('week_start')
 
-    if id_student is not None:
-        if week_start_param:
-            try:
-                week_start = date.fromisoformat(week_start_param)
-            except ValueError:
-                raise Http404("Invalid date format for week_start parameter")
-        else:
-            today = date.today()
-            week_start = today - timedelta(days=today.weekday())
-
-        end_of_week = week_start + timedelta(days=6)
-
-        student_classes = Classroom.objects.filter(
-            students__id_student=id_student,
-            begin_date__lte=end_of_week,
-            end_date__gte=week_start,
-        ).order_by('day_of_week_begin', 'begin_time')
-
-        previous_week_start = week_start - timedelta(days=7)
-        next_week_start = week_start + timedelta(days=7)
-
-        previous_week_start = previous_week_start.strftime("%Y-%m-%d")
-        next_week_start = next_week_start.strftime("%Y-%m-%d")
-
-        context = {
-            'student_classes': student_classes,
-            'start_of_week': week_start,
-            'end_of_week': end_of_week,
-            'previous_week_start': previous_week_start,
-            'next_week_start': next_week_start,
-        }
-        return render(request, 'student/student_schedule.html', context)
+    if week_start_param:
+        try:
+            week_start = date.fromisoformat(week_start_param)
+        except ValueError:
+            raise Http404("Invalid date format for week_start parameter")
     else:
-        request.session['next_url'] = request.path
-        return redirect('student_login')
+        today = date.today()
+        week_start = today - timedelta(days=today.weekday())
+
+    end_of_week = week_start + timedelta(days=6)
+
+    student_classes = Classroom.objects.filter(
+        students__id_student=id_student,
+        begin_date__lte=end_of_week,
+        end_date__gte=week_start,
+    ).order_by('day_of_week_begin', 'begin_time')
+
+    previous_week_start = week_start - timedelta(days=7)
+    next_week_start = week_start + timedelta(days=7)
+
+    previous_week_start = previous_week_start.strftime("%Y-%m-%d")
+    next_week_start = next_week_start.strftime("%Y-%m-%d")
+
+    context = {
+        'student_classes': student_classes,
+        'start_of_week': week_start,
+        'end_of_week': end_of_week,
+        'previous_week_start': previous_week_start,
+        'next_week_start': next_week_start,
+    }
+
+    return render(request, 'student/student_schedule.html', context)
 
 
 @student_required
 def student_profile_view(request):
-    if 'id_student' in request.session:
-        id_student = request.session['id_student']
-        try:
-            student = StudentInfo.objects.get(id_student=id_student)
+    id_student = request.session['id_student']
+    student = StudentInfo.objects.get(id_student=id_student)
 
-            if request.method == 'POST':
-                student.student_name = request.POST['student_name']
-                student.email = request.POST['email']
-                student.phone = request.POST['phone']
-                student.address = request.POST['address']
-                student.birthday = datetime.strptime(request.POST['birthday'], '%d/%m/%Y').date()
-                student.save()
-                messages.success(request, 'Thay đổi thông tin thành công.')
-            context = {'student': student}
-            return render(request, 'student/student_profile.html', context)
-        except StudentInfo.DoesNotExist:
-            return redirect('student_login')
-    else:
-        request.session['next_url'] = request.path
-        return redirect('student_login')
+    if request.method == 'POST':
+        student.student_name = request.POST['student_name']
+        student.email = request.POST['email']
+        student.phone = request.POST['phone']
+        student.address = request.POST['address']
+        student.birthday = datetime.strptime(request.POST['birthday'], '%d/%m/%Y').date()
+        student.save()
+        messages.success(request, 'Thay đổi thông tin thành công.')
+
+    context = {'student': student}
+
+    return render(request, 'student/student_profile.html', context)
 
 
 @student_required
 def student_change_password_view(request):
-    if 'id_student' in request.session:
-        id_student = request.session['id_student']
+    id_student = request.session['id_student']
+    student = StudentInfo.objects.get(id_student=id_student)
 
-        try:
-            student = StudentInfo.objects.get(id_student=id_student)
+    if request.method == 'POST':
+        old_password = request.POST['old_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
 
-            if request.method == 'POST':
-                old_password = request.POST['old_password']
-                new_password = request.POST['new_password']
-                confirm_password = request.POST['confirm_password']
+        if check_password(old_password, student.password):
+            if new_password == confirm_password:
+                student.password = make_password(new_password)
+                student.save()
+                update_session_auth_hash(request, student)
+                messages.success(request, 'Đổi mật khẩu thành công.')
+            else:
+                messages.error(request, 'Mật khẩu mới không khớp.')
+        else:
+            messages.error(request, 'Mật khẩu cũ không đúng.')
 
-                if check_password(old_password, student.password):
-                    if new_password == confirm_password:
-                        student.password = make_password(new_password)
-                        student.save()
-                        update_session_auth_hash(request, student)
-                        messages.success(request, 'Đổi mật khẩu thành công.')
-                    else:
-                        messages.error(request, 'Mật khẩu mới không khớp.')
-                else:
-                    messages.error(request, 'Mật khẩu cũ không đúng.')
-
-            return render(request, 'student/student_change_password.html')
-
-        except StudentInfo.DoesNotExist:
-            return redirect('student_login')
-    else:
-        request.session['next_url'] = request.path
-        return redirect('student_login')
+    return render(request, 'student/student_change_password.html')
