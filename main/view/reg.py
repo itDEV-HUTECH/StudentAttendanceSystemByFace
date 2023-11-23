71ccd2134f1a7f4bdd284513931a0186e52ccb13
Phuc <ngohoangphuc.work@gmail.com>
2023-11-23 17:41:50 +0700

    Update Antispoofing - Phúc

--- main/view/reg.py
+++ main/view/reg.py
@@ -6,7 +6,6 @@ import imutils
 import numpy as np
 import tensorflow as tf
 from imutils.video import VideoStream
-from facenet_pytorch import MTCNN
 from main.src.anti_spoof_predict import AntiSpoofPredict
 from main.src.generate_patches import CropImage
 from main.src.utility import parse_model_name
@@ -19,13 +18,11 @@ import warnings
 
 # Trước khi gọi hàm có cảnh báo
 with warnings.catch_warnings():
-    # Tắt tất cả các cảnh báo
     warnings.simplefilter("ignore")
 
 
 
 
-mtcnn = MTCNN(thresholds= [0.7, 0.7, 0.8] ,keep_all=True, device = 0)
 model_test = AntiSpoofPredict(0)
 image_cropper = CropImage()
 model_dir = "main/resources/anti_spoof_models"
@@ -84,10 +81,10 @@ def insert_attendance(id_classroom, student_id):
 
 
 def draw_progress_bar(frame, progress, x, y, w, h):
-    bar_width = 300
+    bar_width = 150
     bar_height = 20
     bar_x = x
-    bar_y = y + h
+    bar_y = y - 20
     cv2.rectangle(frame, (bar_x, bar_y), (bar_x + bar_width, bar_y + bar_height), (0, 0, 0), -1)
     filled_width = int(bar_width * progress)
     cv2.rectangle(frame, (bar_x, bar_y), (bar_x + filled_width, bar_y + bar_height), (0, 255, 0), -1)
@@ -112,7 +109,6 @@ def main(id_subject):
     images_placeholder = graph.get_tensor_by_name("input:0")
     embeddings = graph.get_tensor_by_name("embeddings:0")
     phase_train_placeholder = graph.get_tensor_by_name("phase_train:0")
-    embedding_size = embeddings.get_shape()[1]
 
 
     cap = cv2.VideoCapture(1)
@@ -203,7 +199,7 @@ def main(id_subject):
                                     if current_face_name != "SUCCESS":
                                         print("Success: Face Recognized as", insert)
                             else:
-                                message = f"{best_name} has already been marked."
+                                message = f"{best_name} da diem danh."
                                 cv2.rectangle(frame, (x, y), (w, h), (0, 0, 255),
                                               2)  # Red bounding box for duplicates
                                 cv2.putText(frame, message, (x, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1,
@@ -219,8 +215,8 @@ def main(id_subject):
                                         cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), thickness=1,
                                         lineType=2)
                 else:
-                    result_text = "FakeFace Score: {:.2f}".format(value)
-                    color = (0, 0, 255)
+                    result_text = "Gia mao !!!".format(value)
+                    color = (0, 255, 255)
                     cv2.rectangle(
                         frame,
                         (image_bbox[0], image_bbox[1] - 50),
@@ -232,7 +228,8 @@ def main(id_subject):
                         frame,
                         result_text,
                         (image_bbox[0], image_bbox[1]),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5 * frame.shape[0] / 1024, color)
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color,thickness=1,
+                                        lineType=2)
 
 
 
