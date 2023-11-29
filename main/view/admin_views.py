e6ae8794039eec4c0173465e91a2f2e39cabe055
NamHuynh <kimdong240502@gmail.com>
2023-11-29 11:03:35 +0700

    fix admin_list_classroom_student_management

--- main/view/admin_views.py
+++ main/view/admin_views.py
@@ -389,8 +389,7 @@ def admin_schedule_get_info(request, id_classroom):
 def admin_list_classroom_student_view(request):
     classroom_per_page = 5
     page_number = request.GET.get('page')
-    list_classrooms = Classroom.objects.filter(
-        studentclassdetails__id_classroom__isnull=False).distinct().order_by('day_of_week_begin', 'begin_time')
+    list_classrooms = Classroom.objects.filter(studentclassdetails__id_classroom__isnull=False).distinct().order_by('day_of_week_begin', 'begin_time')
 
     paginator = Paginator(list_classrooms, classroom_per_page)
     page = paginator.get_page(page_number)
